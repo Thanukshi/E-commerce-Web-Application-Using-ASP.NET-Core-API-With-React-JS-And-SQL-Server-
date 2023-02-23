690af64daed85f3531a59575c1350d5851bf2465
Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2023-02-23 20:17:50 +0530

    Update Response class with new properties

--- EMedicineBackend/EMedicineBackend/Models/ResponseClass.cs
+++ EMedicineBackend/EMedicineBackend/Models/ResponseClass.cs
@@ -4,5 +4,24 @@
     {
         public int StatusCode { get; set; }
         public string StatusMessage { get; set; }
+
+        public List<UsersClass> listUsers { get; set; }
+
+        public UsersClass users { get; set; }
+
+        public List<MedicineClass> listMedicine { get; set; }
+
+        public MedicineClass medicine { get; set; }
+
+        public List<CartClass> listCarts { get; set; }
+
+        public List<OrderClass> listOrders { get; set; }
+
+        public OrderClass order { get; set; }
+
+        public List<OrderItemClass> listOrderItems { get; set; }
+
+        public OrderItemClass orderItem { get; set; }
+
     }
 }
