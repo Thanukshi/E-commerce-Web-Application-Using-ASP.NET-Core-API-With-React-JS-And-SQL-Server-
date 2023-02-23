dfbfe8afb52686e52b83d80214943f7a1f2a1c7d
Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2023-02-23 20:12:28 +0530

    Model for Order

---
+++ EMedicineBackend/EMedicineBackend/Models/OrderClass.cs
@@ -0,0 +1,11 @@
+﻿namespace EMedicineBackend.Models
+{
+    public class OrderClass
+    {
+        public int orderID { get; set; }
+        public int UserID { get; set; }
+        public string OrderNo { get; set; }
+        public decimal OrderTotal { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
