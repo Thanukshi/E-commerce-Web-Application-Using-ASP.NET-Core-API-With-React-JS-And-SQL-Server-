7cbbc717965e98d890cb8872e2b26360ed49385f
Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2023-02-23 19:59:14 +0530

    Create Model for Users

---
+++ EMedicineBackend/EMedicineBackend/Models/UsersClass.cs
@@ -0,0 +1,16 @@
+﻿namespace EMedicineBackend.Models
+{
+    public class UsersClass
+    {
+        public int userID { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserPassword { get; set; }
+        public string Email { get; set; }
+        public decimal Fund { get; set; }
+        public string UserType { get; set; }
+        public int UserStatus { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
