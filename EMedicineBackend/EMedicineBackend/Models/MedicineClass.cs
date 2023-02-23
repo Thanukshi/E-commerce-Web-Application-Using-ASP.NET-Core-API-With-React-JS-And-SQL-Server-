43650d93c49844731e858a398700a66ee4ea4d3c
Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2023-02-23 20:06:14 +0530

    Model for Medicine

---
+++ EMedicineBackend/EMedicineBackend/Models/MedicineClass.cs
@@ -0,0 +1,15 @@
+﻿namespace EMedicineBackend.Models
+{
+    public class MedicineClass
+    {
+        public int medicineID { get; set; }
+        public string MedicineName { get; set; }
+        public string ManufactureName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string ImageUrl { get; set; }
+        public int MedisineStatus { get; set; }
+    }
+}
