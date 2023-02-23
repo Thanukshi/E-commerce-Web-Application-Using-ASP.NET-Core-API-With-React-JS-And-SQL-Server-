bd8a86f9b5b1d4796cabdab96aa3593df32baa9c
Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2023-02-23 20:09:37 +0530

    Model for Cart

---
+++ EMedicineBackend/EMedicineBackend/Models/CartClass.cs
@@ -0,0 +1,13 @@
+﻿namespace EMedicineBackend.Models
+{
+    public class CartClass
+    {
+        public int cartID { get; set; }
+        public int UserID { get; set; }
+        public int MedicineID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Discount { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
